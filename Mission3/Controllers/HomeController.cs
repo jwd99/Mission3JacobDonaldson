1086c01f98f44903bfe3393355a2343ea695d652
Jacob D <jwdonaldson99@gmail.com>
2022-01-13 13:48:47 -0700

    Mission 3 Final

---
+++ Mission3/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mission3.Models;
+
+//Added grade and index view. GradeCalc with post sends back data from the model
+namespace Mission3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GradeCalc()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GradeCalc(GradeCalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission3/Models/GradeCalcModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+//GradeCalc model
+namespace Mission3.Models
+{
+    public class GradeCalcModel
+    {
+        //required and keeps range within 100 for each calc field
+        [Required]
+        [Range(0, 100)]
+        public string Assign { get; set; }
+        // get set makes sure data is taken and returned
+        [Required]
+        [Range(0, 100)]
+        public string Group { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public string Quiz { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public string Exam { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public string Intex { get; set; }
+
+    }
+}
